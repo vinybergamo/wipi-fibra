0dfeb6d26fc711212ef7169c0d8a673dbd557531
mateus gotardi <mateus.gotardi@gmail.com>
2024-07-22 17:01:33 -0300

    add try catch to n8n request

--- src/app/api/n8n/route.ts
+++ src/app/api/n8n/route.ts
@@ -4,12 +4,14 @@ export const POST = async (
     req: Request,
 ) => {
     const body = await req.json();
-    const n8nSend = await fetch(process.env.N8N_URL || '',
-        {
-            method: 'POST',
-            body: JSON.stringify(body)
-        })
-    if (n8nSend.status === 200) {
+    try {
+        await fetch(process.env.N8N_URL || '',
+            {
+                method: 'POST',
+                body: JSON.stringify(body)
+            })
         return NextResponse.json({ message: 'success' }, { status: 200 })
-    } else return NextResponse.json({ message: 'error' }, { status: 400 })
+    } catch (error) {
+        return NextResponse.json({ message: 'error', error }, { status: 400 })
+    }
 };
